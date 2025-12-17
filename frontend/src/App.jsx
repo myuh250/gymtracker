619e98e9364f3443811947187e460c71a7a76d2e
DaronD1709 <110324724+DaronD1709@users.noreply.github.com>
2025-12-18 02:13:13 +0700

    configure main routes and integrate global AIChat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,27 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import WorkoutPage from "./pages/WorkoutPage";
+import AIChat from "./components/AIChat";
+import MainLayout from "./layouts/MainLayout";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="workout" element={<WorkoutPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
+      <AIChat />
+    </>
   );
 }
 
